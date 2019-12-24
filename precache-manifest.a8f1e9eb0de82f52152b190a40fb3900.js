f3bbbd5167201e24afe31bb0c6cc33f51c6f057d
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2019-12-24 00:28:35 -0500

    Updates

---
+++ precache-manifest.a8f1e9eb0de82f52152b190a40fb3900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f757620142a6ecccfa30e6026b78223d",
+    "url": "/hellobuildprueba2/index.html"
+  },
+  {
+    "revision": "79ea541e05169b620ce1",
+    "url": "/hellobuildprueba2/static/css/main.5e0b4066.chunk.css"
+  },
+  {
+    "revision": "31c2466f3476228d4fc0",
+    "url": "/hellobuildprueba2/static/js/2.9e245e7a.chunk.js"
+  },
+  {
+    "revision": "644c17f08bb3807d7a1259f95997d6a8",
+    "url": "/hellobuildprueba2/static/js/2.9e245e7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79ea541e05169b620ce1",
+    "url": "/hellobuildprueba2/static/js/main.caf969c2.chunk.js"
+  },
+  {
+    "revision": "56fa2691de225a8cd670",
+    "url": "/hellobuildprueba2/static/js/runtime-main.6da7d731.js"
+  }
+]);
